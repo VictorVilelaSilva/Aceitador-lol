f6036753198c79bc089f925d5ca5df681e285bbe
Victor de Souza Vilela da Silva <victorsouzavilelasilva@gmail.com>
2024-12-01 03:35:33 -0300

    initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+
+from time import sleep
+import time
+import pyautogui
+
+def runScript():
+    print("Script rodando")
+    while True:
+            pyautogui.locateOnScreen("imgs/NaFila.png",minSearchTime=20000)
+            print("Buscando partida")
+            sleep(1)
+            buttonLocation = pyautogui.locateOnScreen("imgs/Aceitar.png",minSearchTime=240)
+            print(f"Achou partida {time.strftime('%H:%M:%S', time.localtime())}") 
+            buttonPoint = pyautogui.center(buttonLocation)
+            pyautogui.click(buttonPoint)
+            print(f"Partida Aceitada com sucesso {time.strftime('%H:%M:%S', time.localtime())}") 
+            sleep(1)
+            pyautogui.moveTo(0,0)
+            
+if __name__ == "__main__":
+    runScript()
+    print("Script finalizado")
