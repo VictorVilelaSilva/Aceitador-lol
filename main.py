345a8917f7550feeb2b3bea4efc6946beb694d80
Victor de Souza Vilela da Silva <victorsouzavilelasilva@gmail.com>
2024-12-01 13:43:18 -0300

    hot fix joao vitor

--- main.py
+++ main.py
@@ -6,16 +6,16 @@ import pyautogui
 def runScript():
     print("Script rodando")
     while True:
-            pyautogui.locateOnScreen("imgs/NaFila.png",minSearchTime=20000)
-            print("Buscando partida")
-            sleep(1)
-            buttonLocation = pyautogui.locateOnScreen("imgs/Aceitar.png",minSearchTime=960)
-            print(f"Achou partida {time.strftime('%H:%M:%S', time.localtime())}") 
-            buttonPoint = pyautogui.center(buttonLocation)
-            pyautogui.click(buttonPoint)
-            print(f"Partida aceita com sucesso {time.strftime('%H:%M:%S', time.localtime())}") 
-            sleep(1)
-            pyautogui.moveTo(0,0)
+        pyautogui.locateOnScreen("imgs/NaFila.png",minSearchTime=20000)
+        print("Buscando partida")
+        sleep(1)
+        buttonLocation = pyautogui.locateOnScreen("imgs/Aceitar.png",minSearchTime=960)
+        print(f"Achou partida {time.strftime('%H:%M:%S', time.localtime())}") 
+        buttonPoint = pyautogui.center(buttonLocation)
+        pyautogui.click(buttonPoint)
+        print(f"Partida aceita com sucesso {time.strftime('%H:%M:%S', time.localtime())}") 
+        sleep(1)
+        pyautogui.moveTo(0,0)
             
 if __name__ == "__main__":
     runScript()
